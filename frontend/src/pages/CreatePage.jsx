a93fae46af21098f91a0720b3a7c18dee3eaf8ac
ramneekriar <ramneekriar@gmail.com>
2025-01-07 00:08:17 -0500

    Add toast indicating whether product was added to store or not

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -8,6 +8,7 @@ import {
   useColorModeValue,
   Input,
   Button,
+  useToast,
 } from '@chakra-ui/react';
 import { useProductStore } from '../store/product';
 
@@ -18,11 +19,28 @@ const CreatePage = () => {
     image: '',
   });
 
+  const toast = useToast();
+
   const { createProduct } = useProductStore();
+
   const handleAddProduct = async () => {
     const { success, message } = await createProduct(newProduct);
-    console.log('Success:', success);
-    console.log('Message:', message);
+    if (!success) {
+      toast({
+        title: 'Error',
+        description: message,
+        status: 'error',
+        isClosable: true,
+      });
+    } else {
+      toast({
+        title: 'Success',
+        description: message,
+        status: 'success',
+        isClosable: true,
+      });
+    }
+    setNewProduct({ name: '', price: '', image: '' });
   };
 
   return (
